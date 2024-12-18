a2385de18419146ccbbabf5ad164131c2fa8200c
excretory <elstxds@proton.me>
2024-12-18 23:09:22 +0500

    Alembic init

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from contextlib import asynccontextmanager
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_h.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+
     yield
 
 
